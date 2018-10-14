7be9a9dd061d30c25241f831d6044b9dde605cb3
Todd Gamblin <tgamblin@llnl.gov>
2018-10-14 15:21:57 -0700

    env: add spack env activate/deactivate and shell support
    - `spack env activate foo`: sets SPACK_ENV to the current active env name
    
    - `spack env deactivate`: unsets SPACK_ENV, deactivates the environment
    
    - added support to setup_env.sh and setup_env.csh
    
    - other env commands work properly with SPACK_ENV, as with an environment
      arguments.
    
    - command-line --env arguments take precedence over the active
      environment, if given.

--- lib/spack/spack/test/cmd/env.py
+++ lib/spack/spack/test/cmd/env.py
@@ -31,7 +31,7 @@ import llnl.util.filesystem as fs
 
 import spack.modules
 import spack.environment as ev
-from spack.cmd.env import _environment_concretize, _environment_create
+from spack.cmd.env import _env_concretize, _env_create
 from spack.version import Version
 from spack.spec import Spec
 from spack.main import SpackCommand
@@ -189,7 +189,7 @@ def test_to_lockfile_dict():
 def test_env_repo():
     e = ev.Environment('testx')
     e.add('mpileaks')
-    _environment_concretize(e)
+    _env_concretize(e)
 
     package = e.repo.get(spack.spec.Spec('mpileaks'))
     assert package.namespace == 'spack.pkg.builtin.mock'
@@ -265,7 +265,7 @@ env:
 """
     spack.package_prefs.PackagePrefs.clear_caches()
 
-    _environment_create('test', test_config)
+    _env_create('test', test_config)
 
     e = ev.read('test')
     ev.prepare_config_scope(e)
@@ -285,7 +285,7 @@ env:
 """
     spack.package_prefs.PackagePrefs.clear_caches()
 
-    _environment_create('test', test_config)
+    _env_create('test', test_config)
 
     e = ev.read('test')
 
@@ -315,7 +315,7 @@ env:
 """ % config_scope_path
 
     spack.package_prefs.PackagePrefs.clear_caches()
-    _environment_create('test', test_config)
+    _env_create('test', test_config)
 
     e = ev.read('test')
 
@@ -347,7 +347,7 @@ env:
 """
     spack.package_prefs.PackagePrefs.clear_caches()
 
-    _environment_create('test', test_config)
+    _env_create('test', test_config)
 
     e = ev.read('test')
 
