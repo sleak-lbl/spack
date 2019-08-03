1a678b61ae2fbeab80e913a7ca0d874997e455a8
Woo-Sun Yang <wyang@lbl.gov>
2019-08-03 08:55:22 -0700

    Update scorep otf2 opari2 cubew cubelib

--- var/spack/repos/builtin/packages/scorep/package.py
+++ var/spack/repos/builtin/packages/scorep/package.py
@@ -15,6 +15,7 @@ class Scorep(AutotoolsPackage):
     homepage = "http://www.vi-hps.org/projects/score-p"
     url      = "https://www.vi-hps.org/cms/upload/packages/scorep/scorep-4.1.tar.gz"
 
+    version('6.0', sha256='5dc1023eb766ba5407f0b5e0845ec786e0021f1da757da737db1fb71fc4236b8')
     version('5.0',   '0651614eacfc92ffbe5264a3efebd0803527ae6e8b11f7df99a56a02c37633e1')
     version('4.1',   '7bb6c1eecdd699b4a3207caf202866778ee01f15ff39a9ec198fcd872578fe63')
     version('4.0',   'f04478e0407d67eeb8c49c3c51d91e12')
